5ff4d4fed1498f5dea0ac59bc05568aeb6577e0d
duka88 <djankov88@gmail.com>
2021-09-12 19:59:04 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+  {
+    id: 1,
+    title: "Zatamnjenje",
+    author: "Ed Brubejker",
+    img: "https://carobnaknjiga.rs/chest/pimg/1628687699_Naslovna%20sajt.jpg",
+  },
+  {
+    id: 2,
+    title: "Y, poslednji muškarac",
+    author: "Brajan K. Von",
+    img: "https://carobnaknjiga.rs/chest/pimg/1630395370_Y%20-%20naslovna%20sajt%202.jpg",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <section className="booklist">
+      {books.map((item) => {
+        return <Book {...item} key={item.id} />;
+      })}
+    </section>
+  );
+};
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4 style={{}}>{author}</h4>
+    </article>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
