d4c6d35b66d068d163b58dde7501a087f8293baf
duka88 <djankov88@gmail.com>
2021-09-17 20:54:46 +0200

    custom hooks

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,31 +1,37 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
+import { reducer } from "./reducer";
 
-// reducer function
-const reducer =(state, action) => {
-
-}
 const defaultState = {
-  people:[],
+  people: [],
   isModalOpen: false,
-  modalContent: 'hello world'
-}
+  modalContent: "hello world",
+};
 const Index = () => {
   const [name, setName] = useState("");
   const [state, dispatch] = useReducer(reducer, defaultState);
- 
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-     
+      const newPerson = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newPerson });
+      setName("");
     } else {
-     
+      dispatch({ type: "NO_VALUE" });
     }
   };
+
+  const closeModal = (e) => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+
   return (
     <>
-      {state.isModalOpen && <Modal />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -41,9 +47,9 @@ const Index = () => {
           <div key={person.id} className="item">
             <h4>{person.name}</h4>
             <button
-              // onClick={() =>
-              //   dispatch({ type: "REMOVE_ITEM", payload: person.id })
-              // }
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
             >
               remove
             </button>
--- src/tutorial/6-useReducer/setup/reducer.js
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -1,3 +1,32 @@
 export const reducer = (state, action) => {
-
+  if (action.type === "ADD_ITEM") {
+    const newItems = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newItems,
+      isModalOpen: true,
+      modalContent: "person added",
+    };
+  }
+  if (action.type === "NO_VALUE") {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: "enter value",
+    };
+  }
+  if (action.type === "CLOSE_MODAL") {
+    return {
+      ...state,
+      isModalOpen: false,
+      modalContent: "",
+    };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    const newitems = state.people.filter((item) => item.id !== action.payload);
+    return {
+      ...state,
+      people: newitems,
+    };
+  }
 };
