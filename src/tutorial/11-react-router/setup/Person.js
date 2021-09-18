79d1cc98c1991c411ca582654446048926255776
duka88 <djankov88@gmail.com>
2021-09-18 20:43:38 +0200

    use memo

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
+  const { id } = useParams();
+  const [name, setName] = useState("default name");
+
+  useEffect(() => {
+    const person = data.find((item) => item.id === +id);
+    setName(person.name);
+  }, []);
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
     </div>
   );
 };
