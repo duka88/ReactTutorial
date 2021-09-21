e995122b52512ee0bb1aa5cbf615da481dcf6913
duka88 <djankov88@gmail.com>
2021-09-21 21:17:44 +0200

    menu

--- projects/05-menu/setup/src/App.js
+++ projects/05-menu/setup/src/App.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+const cat = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
-  return <h2>menu project setup</h2>;
+  const [cats, setCats] = useState(cat);
+  const [prods, setProds] = useState(items);
+  const filter = (cat) => {
+    if (cat === "all") {
+      setProds(items);
+    } else {
+      const filterItems = items.filter((item) => item.category === cat);
+      setProds(filterItems);
+    }
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={cats} filter={filter} />
+        <Menu prods={prods} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
