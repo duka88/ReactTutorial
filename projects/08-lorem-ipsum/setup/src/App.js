e63930b2142593159eab345197a7543a58e808e1
duka88 <djankov88@gmail.com>
2021-09-23 21:16:22 +0200

    color generator

--- projects/08-lorem-ipsum/setup/src/App.js
+++ projects/08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [par, setPar] = useState([]);
+  const [show, setShow] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (show > data.length) {
+      setPar(data);
+      return;
+    }
+    if (show > 0) {
+      const pars = data.slice(0, show);
+      setPar(pars);
+    }
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={show}
+          onChange={(e) => setShow(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {par.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
